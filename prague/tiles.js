692b47daf9dc8bd5a29f11e7cf5182fd7378afc3
Tomas Bergl <61440072+TomasBergl@users.noreply.github.com>
2022-05-24 16:46:28 +0200

    Add test support for Prague pages

---
+++ prague/tiles.js
@@ -0,0 +1,284 @@
+const technicalSupportTiles = [
+    {
+        name: 'Technical Support',
+        sections: [
+            {
+                people: [
+                    { name: 'Martin Drapal', id: 'U02S4A6FJCW-90abe5a327c8', isEM: true },
+                    { name: 'Alain Tanke Tsayem', id: 'U01S9NGUV71-d5c73cd887b0', },
+                    { name: 'Zsolt Petro', id: 'U02BJJLCTE0-9e1b1f0e94ea', },
+                    { name: 'Bojan Viculin', id: 'U02C7HYGJSJ-08a3d934c9b7', },
+                    { name: 'Romain Dartois', id: 'U02GNMR9QQ5-f4c05a8dc6f0', },
+                    { name: 'Victor Acevedo Donato', id: 'U02PAAPMTAR-f22c135224fe', },
+                    { name: 'Ayesha Naweed', id: 'U02RJLMQDDH-b223ae395062', },
+                    { name: 'Ahmed Ibrahim', id: 'U02RT9T2HDJ-4126a75fc72f', },
+                    { name: 'Bea Munoz', id: 'U02RL8WK11T-81f47999feec', },
+                    { name: 'Valentin Bala', id: 'U0308NF44GP-855ef2d91f96', },
+                ]
+            },
+        ]
+    }
+];
+
+const otherTiles = [
+    {
+        name: 'People & Facilities',
+        sections: [
+            {
+                people: [
+                    { name: 'Milada Kohlerova', id: 'U01UFATJ738-682875592887', },
+                    { name: 'Lucie Michaliková', id: 'U034UD1F6CU-9ca87aa0455b', },
+                    { name: 'Viktoria Silleova', id: 'U026K7HT2UQ-20d94384ef05', },
+                ]
+            },
+        ]
+    },
+    {
+        name: 'Recruitment',
+        sections: [
+            {
+                people: [
+                    { name: 'Raluca Groues', id: 'U01NB0617B6-750cee888d5a', },
+                ]
+            },
+        ]
+    },
+    {
+        name: 'IT',
+        sections: [
+            {
+                people: [
+                    { name: 'Fredrik Cronqvist', id: 'U033V7WH6G0-a7b8b8aff054', },
+                ]
+            },
+        ]
+    },
+];
+
+const pdeTiles = [
+    {
+        name: 'Email',
+        sections: [
+            {
+                name: 'Experiences',
+                key: 'EE',
+                people: [
+                    { name: 'AK', id: 'U019Q7PVCAW-824c5bf93bff', isEM: true },
+                    { name: 'Lucas Sartori', id: 'U030PADDD33-ebae7a11bd6d', },
+                    { name: 'Neven Friedrich', id: 'U0300RNNNG4-ce38ee897cc4', },
+                    { name: 'Jakub Kovarik', id: 'U02JJ8FJP71-1c64e97d78d9', },
+                    { name: 'Richard Fridrich', id: 'U022WUR7VHB-b54c9584727d', },
+                    { name: 'Alex Pysanets', id: 'U01GTGU1Y8P-06cd774f6443', isPM: true },
+                ]
+            },
+            {            
+                name: 'Services',
+                key: 'ES',
+                people: [
+                    { name: 'Pavel Smejkal', id: 'U010Q919EMQ-1c0ce831c8e4', isEM: true },
+                    { name: 'Fero Kaduk', id: 'U019Q7PVCAW-7d0c66ca4b87', },
+                    { name: 'Adrian Andrascik', id: 'U01HLUQ5ZLH-59f185893e77', },
+                    { name: 'Alexandr Kara', id: 'U017AFRNH70-98975edf6ed9', },
+                    { name: 'Ilya Nikitin', id: 'U02FEUQ6B63-c137bf9413d3', },
+                    { name: 'Alex Pysanets', id: 'U01GTGU1Y8P-06cd774f6443', isPM: true },
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Calendar',
+        sections: [
+            {
+                name: 'Experiences',
+                people: [
+                    { name: 'Petr Komarek', id: 'U01J8H1Q8RG-6013e0e4e950', isEM: true },
+                    { name: 'Matej Suta', id: 'U01H8G73BKM-e52fee18a0cd', },
+                    { name: 'Dima An', id: 'U01NUL0H6N4-865435e507af', },
+                    { name: 'Dmitrii Kashin', id: 'U0337QPR3NY-cfafbe9d46eb', },
+                    { name: 'David Jambor', id: 'U02DGHJTPQ8-5b64b24786b4', isDSG: true },
+                    { name: 'Sofiya Huts', id: 'U01LJ7KF37X-dd0ccaa14f2b', isPM: true },
+                ]
+            },
+            {
+                name: 'Services',
+                key: 'CALS',
+                people: [
+                    { name: 'Pavel Smejkal', id: 'U010Q919EMQ-1c0ce831c8e4', isEM: true },
+                    { name: 'Adrian Magdas', id: 'U02CSUJ1R17-63207fefb410', },
+                    { name: 'Dan Amaroso', id: 'U0247E2ABUM-3ca2ae681961', },
+                    { name: 'Sofiya Huts', id: 'U01LJ7KF37X-dd0ccaa14f2b', isPM: true },
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Content',
+        sections: [
+            {
+                name: 'Experiences',
+                people: [
+                    { name: 'Jakub Tomanek', id: 'U010K7U35UM-9b5a7cc3b805', isEM: true },
+                    { name: 'Lenka Urban', id: 'U030PADDD33-ebae7a11bd6d', },
+                    { name: 'Cyrus Smith', id: 'U0300RNNNG4-ce38ee897cc4', },
+                    { name: 'Felix Osiobor', id: 'U02RGG9PNA2-74df2f9550a1', isDSG: true },
+                ]
+            },
+            {
+                name: 'Services',
+                people: [
+                    { name: 'TBH', id: 'U010Q919EMQ-1c0ce831c8e5', isEM: true },
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Record Management',
+        sections: [
+            {
+                name: 'Experiences',
+                key: 'RME',
+                people: [
+                    { name: 'Predrag Jovanovic', id: 'U02CLGB8DRA-ae5f75ffd8a4', isEM: true },
+                    { name: 'Jakub Miskech', id: 'U01EGR20S4F-b359078dab67', },
+                    { name: 'Iryna Kukurika', id: 'U03BSRSKZ7C-b2513316647b', },
+                    { name: 'Ali Zdhanov', id: 'U0298GL2DLJ-20a65c87f86f', },
+                    { name: 'Rado Sivak', id: 'U02R9G35CJX-29ef53bbb24a', isDSG: true },
+                ]
+            },
+            {
+                name: 'Services',
+                key: 'RMS',
+                people: [
+                    { name: 'Predrag Jovanovic', id: 'U02CLGB8DRA-ae5f75ffd8a4', isEM: true },
+                    { name: 'Miroslav Stoklasa', id: 'U02D3US15NU-f8defb61cb72', },
+                    { name: 'Karlis Melderis', id: 'U02RL8YLSBF-f4d0eb01d4c0', },
+                    { name: 'Daniel Milde', id: 'U0300RFJF1A-aa80d6dafb8d', },
+                    { name: 'Jakub Štilec', id: 'U0335FAHAFL-763e9c8b98e2', },
+                    { name: 'Daniel Mircea', id: 'U033TAK1087-e3233daab0a8', },
+                    { name: 'Jozef Lami', id: 'U036T79QMME-8991a8eb3614', },
+                    { name: 'Ilya Korolev', id: 'U037ACJT64F-8999943c7004', },
+                    { name: 'Radovan Smitala', id: 'U037ACFRSGP-gf5bc5656821', },
+                    { name: 'Miguel Chambergo', id: 'U03CAU10KDW-geeab00036ad', isCON: true },
+                    { name: 'Alex Pysanets', id: 'U01GTGU1Y8P-06cd774f6443', isPM: true },
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Extensibility',
+        sections: [
+            {
+                name: 'Experiences',
+                key: 'OREE',
+                people: [
+                    { name: 'Taras Perevorskyj', id: 'U03ANE5HL2K-ec23f1e8677e', isEM: true },
+                    { name: 'Tomas Voslar', id: 'U024Q8NFSFR-4f27f4427ce2', },
+                    { name: 'Alex Shevchenko', id: 'U025RKM4X2M-f90453f830be', },
+                    { name: 'Vojtech Sleichert', id: 'U02FVLCDQR1-045daefb52d9', },
+                    { name: 'Nikola Malovic', id: 'U012J81FT5F-4082830a162f', },
+                    { name: 'Palo Otcenas', id: 'U02VCGK49L2-0ec2bdb17cd4', },
+                    { name: 'Denis Rojcyk', id: 'U02C7HZTZGE-e7c8fbec20cc', isDSG: true },
+                ]
+            },
+            {
+                name: 'Services',
+                key: 'ORES',
+                people: [
+                    { name: 'Petr Smejkal', id: 'U032H69E13P-0eb4a937d709', isEM: true },
+                    { name: 'Genna Altukhov', id: 'U01CT2TD0CV-ef647f55541e', },
+                    { name: 'Gabriel Gilini', id: 'U01R9MZBA1M-61c0129cce36', },
+                    { name: 'Anton Audzei', id: 'U01R9MZBA1M-61c0129cce36', },
+                    { name: 'Gerardo Perez', id: 'U01SY7UU78R-c87dd96d062a' },
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Extension Projects',
+        key: 'EP',
+        sections: [
+            {
+                people: [
+                    { name: 'Pavel Studeny', id: 'UUFT3ANUF-0d0e7071e3e3', isEM: true },
+                    { name: 'Milan Konir', id: 'U01B92H8DNG-b12c9d13fbe0', },
+                    { name: 'Kornel Dubieniecki', id: 'U01U7AX6A6T-f95e27224762', },
+                    { name: 'Samuel Kollat', id: 'U017DG9KUQ3-dff364496a53', },
+                    { name: 'Maurizio Pucci', id: 'U018P88G06S-050631b42da5', },
+                    { name: 'Anton Magdziak', id: 'U01LTV25R61-92fd25acb43f', isCON: true },
+                    { name: 'Dmytro Korlotian', id: 'U0298GPCVV0-d34059e3d0be', isDSG: true },
+                    { name: 'Aneta Dolezelova', id: 'U02K2Q3BFJQ-0433f62cc767', isPM: true },
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Admin Experiences',
+        sections: [
+            {
+                people: [
+                    { name: 'Armen Ayvazyan', id: 'U01CPDC27ML-b376efb672a6', isEM: true },
+                    { name: 'Martin Petrla', id: 'U018VA2V6E5-91db3344ed61', },
+                    { name: 'Kirill Glebov', id: 'U019FGB550F-45932faff201', },
+                    { name: 'Jan Kubat', id: 'U01F28TB3EZ-3e3be0e4e2ae', },
+                    { name: 'Galen Nedelchev', id: 'U03D91LQ70A-20de024639ce', },
+                    { name: 'Rasmus Josefsson', id: 'U01EQRAPW3G-07aa70c3aed7', },
+                    { name: 'Vladlena Svermova', id: 'U01H57KSANR-f4e69cc6c2e3', },
+                    { name: 'Marina Middleton', id: 'U01R9N071PZ-d57e81cfca38', },
+                    { name: 'Jerry Tvrdek', id: 'U03D7G7UEQH-09379da551eb', isDSG: true },
+                    { name: 'Tomas Zeman', id: 'U01BWJG6X5X-253185f59cac', isPM: true },
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Search',
+        sections: [
+            {
+                people: [
+                    { name: 'Tomas Vlach', id: 'U01EYKA7HC6-fc8173d1acff', isEM: true },
+                    { name: 'Alexandr Maximov', id: 'U01VDAE7YTX-50ff7cf2f75e', },
+                    { name: 'Jan Hajek', id: 'U01UN48CQJE-d7f8ad9e0605', },
+                    { name: 'Pavlo Dvorknikov', id: 'U0335FDUS9Y-0edfc44ab0b7', },
+                    { name: 'Vladimir Gabrielyan', id: 'U0337QPG1KN-81972f3f2b81', },
+                ]
+            }
+        ]
+    },
+    {
+        name: 'COR team',
+        key: 'COR',
+        sections: [
+            {
+                people: [
+                    { name: 'Richard Shaw', id: 'U014JJP7Y4B-15e254d44e79', isEM: true },
+                    { name: 'Zdenek Vicar', id: 'U01RHMTCVJA-90bd00709751', },
+                    { name: 'Jan Ludvik', id: 'U02ARHQQ3MK-53cdc28ba026', },
+                    { name: 'Danil Vagapov', id: 'U0383EPQ7L6-a303af2c28cf', },
+                ]
+            }
+        ]
+    },
+    {
+        name: 'QF',
+        key: 'QF',
+        sections: [
+            {
+                people: [
+                    { name: 'Jan Gahura', id: 'U020T0A677D-b9a9dbd6ae59', isEM: true },
+                    { name: 'Erik Witkowski', id: 'U029ENNEB9A-37bf263343d1', },
+                    { name: 'Tomas Susovsky', id: 'U02CLGB0DNG-5d26713ff6d8', },
+                    { name: 'Marek Fexa', id: 'U02RHRJB6BX-2991caf459fe', },
+                ]
+            }
+        ]
+    },
+];
+
+const allTiles = pdeTiles.concat(technicalSupportTiles).concat(otherTiles);
+
+const tabs = [
+    { label: 'PDE', tiles: pdeTiles }, 
+    { label: 'TECHNICAL SUPPORT', tiles: technicalSupportTiles }, 
+    { label: 'PEOPLE & FACILITIES', tiles: otherTiles }, 
+    { label: 'ALL', tiles: allTiles }, 
+];
